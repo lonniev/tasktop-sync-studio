8c85c0ada2f2b8d6fbd5e5b2c317bbe28e3e7f48
Lonnie VanZandt <lonniev@gmail.com>
2014-12-12 13:55:03 -0700

    Have to retrieve remote file first

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,14 @@
 # limitations under the License.
 #
 
-windows_package 'Google Chrome' do
-  source 'https://dl-ssl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B806F36C0-CB54-4A84-A3F3-0CF8A86575E0%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dfalse/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi'
+chromeFile = Pathname.new( "#{Chef::Config[:file_cache_path]}" ).join( "GoogleChromeStandaloneEnterprise.msi" )
+
+remote_file chromeFile.to_s do
+  source "https://dl-ssl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B806F36C0-CB54-4A84-A3F3-0CF8A86575E0%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dfalse/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
+  action :create_if_missing
+end
+
+package 'Google Chrome' do
+  source chromeFile.to_s
   action :install
 end
