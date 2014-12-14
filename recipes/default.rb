2f25123ce79792864c8f4b8312d6d1a5da5e0e4f
Lonnie VanZandt <lonniev@gmail.com>
2014-12-13 17:40:16 -0700

    Retry opscode/windows from their master commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,7 +17,7 @@
 # limitations under the License.
 #
 
-chromeFile = Pathname.new( "#{Chef::Config[:file_cache_path]}" ).join( "GoogleChromeStandaloneEnterprise.msi" )
+chromeFile = Pathname.new( Chef::Config[:file_cache_path] ).join( "GoogleChromeStandaloneEnterprise.msi" )
 
 remote_file chromeFile.to_s do
   source "https://dl-ssl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B806F36C0-CB54-4A84-A3F3-0CF8A86575E0%7D%26lang%3Den%26browser%3D3%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dfalse/edgedl/chrome/install/GoogleChromeStandaloneEnterprise.msi"
@@ -33,4 +33,30 @@ dosPath = chromeFile.expand_path().to_s.gsub( '/', '\\' )
 execute 'Install Chrome' do
   command "msiexec /qn /i #{dosPath}"
   action :run
+end
+
+package 'wget' do
+  source 'http://sourceforge.net/projects/gnuwin32/files/wget/1.11.4-1/wget-1.11.4-1-setup.exe/download'
+  action :install
+end
+
+# Using wget to download content protected by referer and cookies.
+# 1. get base url and save its cookies in file
+# 2. get protected content using stored cookies
+
+repoSite = 'https://files.tasktop.com/public.php'
+repoId = '7d481ea5e31a0a39b5d9eade8c90697e'
+repoPass = 'foo'
+
+filePath = '//Sodius-Engineering.license'
+asLocalFile = 'Sodius-Engineering.license'
+
+remote_file = "#{repoSite}?service=files&t=#{repoId}&download&path=#{filePath}"
+
+execute 'authenticate at Tasktop and save session cookies' do
+  command 'wget --post-data "password=#{repoPass}" --no-check-certificate --cookies=on --keep-session-cookies --save-cookies=#{cookieFile} "#{repoSite}?service=files&t=#{repoId}" -O #{junkFile}'
+end
+
+execute 'download remote file with session cookies' do
+  command 'wget -O #{asLocalFile} --referer=#{repoSite} --cookies=on --load-cookies=#{cookieFile} --keep-session-cookies --save-cookies=#{cookieFile} #{remoteFile}'
 end
